58ae1968a604607e97be51e8cd6cc6ec52f6efd6
Vitaly <u14111415232119518@gmail.com>
2024-04-20 10:19:23 +0300

    Написал задачи на ближайшую неделю. Написал пример кода с использованием библиотека socket.

---
+++ server_and_drone_communication_api/client_(socket).py
@@ -0,0 +1,24 @@
+import socket
+
+# Создаем сокет
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Подключаемся к серверу
+server_address = ('localhost', 10000)
+sock.connect(server_address)
+
+try:
+    # Отправляем данные на сервер
+    message = input('Enter your message: ')
+    sock.sendall(message.encode())
+
+    # Получаем ответ от сервера
+    amount_received = 0
+    amount_expected = len(message)
+    while amount_received < amount_expected:
+        data = sock.recv(1024)
+        amount_received += len(data)
+        print('Received "%s"' % data)
+finally:
+    # Закрываем соединение
+    sock.close()
